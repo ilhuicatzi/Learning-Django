07548e1e8af76a10f75c2b897bdb7c1b3d6537c8
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2025-01-03 23:55:07 -0600

    Add TypeScript interfaces for Chapter and Data models

---
+++ src/types/chapters.d.ts
@@ -0,0 +1,15 @@
+export interface Chapter {
+    id:         string;
+    slug:       string;
+    body:       string;
+    collection: string;
+    data:       Data;
+}
+
+export interface Data {
+    id:       string;
+    title:    string;
+    subtitle: string;
+    date:     Date;
+    url:      string;
+}
